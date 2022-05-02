f1b46ec4552bb10c13b240defcbfcdad82f62ebf
BONVIN Adrien - m106492 <adrien.bonvin@soprasteria.com>
2022-05-02 16:35:29 +0200

    Modification serveur.js

--- serveur.js
+++ serveur.js
@@ -3,10 +3,10 @@ const path = required('path');
 
 const app = express();
 
-app.use(express.static(_dirname + '/dist/myApp'))
+app.use(express.static(__dirname + '/dist/myApp'))
 
 app.length('/*', function(req,res){
-  res.sendFile(path.join(_dirname + '/dist/myApp/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/myApp/index.html'))
 });
 
 app.listen(process.env.PORT);
